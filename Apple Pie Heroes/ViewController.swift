68d1fca524d62e2c33fc97665e34b78557f21931
Egor Astakhov <noise718@gmail.com>
2020-10-30 13:41:00 +0300

    Initial Commit

---
+++ Apple Pie Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie Heroes
+//
+//  Created by Егор Астахов on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
